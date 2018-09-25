23bb50a3792e1c7e81e1377651760440e19d3cba
Celal Akyüz <celal.akyuz@creapeak.com>
2018-09-25 22:55:18 +0300

    cms

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -1,1 +1,32 @@
-test ürün
+<div class="row">
+    <div class="col-md-12">
+        <h4 class="m-b-lg">Ürün Listesi</h4>
+    </div><!-- END column -->
+    <div class="col-md-12">
+        <div class="widget p-lg">
+            <table class="table table-hover table-striped">
+                <thead>
+                    <th>#ID</th>
+                    <th>URL</th>
+                    <th>Başlık</th>
+                    <th>Açıklama</th>
+                    <th>Durum</th>
+                    <th>İşlem</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>monitor-askisi</td>
+                        <td>Monitör Askısı</td>
+                        <td>360 derece kullanılabilen monitör askısıdır...</td>
+                        <td>1/0</td>
+                        <td>
+                            <a href="#" class="btn btn-xs btn-danger btn-outline"><i class="fa fa-trash"></i> Sil</a>
+                            <a href="#" class="btn btn-xs btn-info btn-outline"><i class="fa fa-edit"></i> Düzenle</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
