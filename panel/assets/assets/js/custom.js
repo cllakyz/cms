4400d11fbde2a63f331fcd092b5600b14ad3b195
Celal Akyüz <celal.akyuz@creapeak.com>
2018-09-29 18:29:00 +0300

    cms

--- panel/assets/assets/js/custom.js
+++ panel/assets/assets/js/custom.js
@@ -1,3 +1,20 @@
 $(document).ready(function () {
-    alert();
+    $(document).on('click', '.remove-btn', function (e) {
+        e.preventDefault();
+        var url = $(this).attr('data-url');
+        swal({
+            title: 'Emin misiniz?',
+            text: "Bu kaydı silmek istediğinize emin misiniz?",
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            cancelButtonText: 'İptal',
+            confirmButtonText: 'Evet, Sil!'
+        }).then((result) => {
+            if (result.value) {
+                window.location.href = url;
+            }
+        })
+    });
 });
