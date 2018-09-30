7b4fee57fc46db343cb33ec12956b6255d4137db
Celal Akyüz <celal.akyuz@creapeak.com>
2018-09-30 19:03:14 +0300

    cms

--- panel/application/views/product_v/image/content.php
+++ panel/application/views/product_v/image/content.php
@@ -1,8 +1,5 @@
 <div class="row">
     <div class="col-md-12">
-        <h4 class="m-b-lg">Ürünün Fotoğrafları</h4>
-    </div><!-- END column -->
-    <div class="col-md-12">
         <div class="widget">
             <div class="widget-body">
                 <form action="../api/dropzone" class="dropzone" data-plugin="dropzone" data-options="{ url: '../api/dropzone'}">
@@ -25,24 +22,24 @@
             <div class="widget-body">
                 <table class="table table-bordered table-striped table-hover">
                     <thead>
-                        <th>#ID</th>
-                        <th>Görsel</th>
+                        <th class="text-center">#ID</th>
+                        <th class="text-center">Görsel</th>
                         <th>Görsel Adı</th>
-                        <th>Durumu</th>
-                        <th>İşlem</th>
+                        <th class="text-center">Durumu</th>
+                        <th class="text-center">İşlem</th>
                     </thead>
                     <tbody>
                         <tr>
-                            <td>#1</td>
-                            <td>
+                            <td class="w100 text-center">#1</td>
+                            <td class="w100">
                                 <img width="30" src="http://dev.newcms.com/panel/assets/assets/images/221.jpg" alt="" class="img-responsive">
                             </td>
                             <td>deneme-urun.jpg</td>
-                            <td>
+                            <td class="w100 text-center">
                                 <input type="checkbox" class="change-item-status" data-url="<?php echo base_url('product/change_status/'); ?>" data-switchery data-color="#10c469"<?php echo 1 == 1 ? ' checked' : NULL; ?> />
                             </td>
-                            <td>
-                                <a data-url="<?php echo base_url('product/delete/'. 1); ?>" class="btn btn-sm btn-danger btn-outline remove-btn"><i class="fa fa-trash"></i> Sil</a>
+                            <td class="w100 text-center">
+                                <a data-url="<?php echo base_url('product/delete/'. 1); ?>" class="btn btn-sm btn-danger btn-block btn-outline remove-btn"><i class="fa fa-trash"></i> Sil</a>
                             </td>
                         </tr>
                     </tbody>
