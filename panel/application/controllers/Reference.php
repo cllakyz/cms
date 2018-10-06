df72f2a280341348a7dad55382a920d8831b8277
Celal Akyüz <celal.akyuz@creapeak.com>
2018-10-06 23:51:00 +0300

    cms

---
+++ panel/application/controllers/Reference.php
@@ -0,0 +1,365 @@
+<?php
+
+class Reference extends CI_Controller
+{
+    public $viewFolder = "";
+    private $zaman = "";
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->viewFolder = "reference_v";
+        $this->load->model("reference_model");
+        $this->zaman = date('Y-m-d H:i:s');
+    }
+    /* anasayfa */
+    public function index()
+    {
+        $viewData = new stdClass();
+        /** Tablodan verilerin getirilmesi */
+        $items = $this->reference_model->get_all(array(), "rank ASC");
+
+        $viewData->viewFolder = $this->viewFolder;
+        $viewData->subViewFolder = "list";
+        $viewData->items = $items;
+
+        $this->load->view($viewData->viewFolder.'/'.$viewData->subViewFolder.'/index', $viewData);
+    }
+    /* yeni kayıt form */
+    public function new_form()
+    {
+        $viewData = new stdClass();
+
+        $viewData->viewFolder = $this->viewFolder;
+        $viewData->subViewFolder = "add";
+
+        $this->load->view($viewData->viewFolder.'/'.$viewData->subViewFolder.'/index', $viewData);
+    }
+    /* düzenle form */
+    public function edit_form($id)
+    {
+        $viewData = new stdClass();
+        /** Tablodan verilerin getirilmesi */
+        $item = $this->reference_model->get(
+            array(
+                'id' => strip_tags(str_replace(' ', '', $id))
+            )
+        );
+
+        $viewData->viewFolder = $this->viewFolder;
+        $viewData->subViewFolder = "edit";
+        $viewData->item = $item;
+
+        $this->load->view($viewData->viewFolder.'/'.$viewData->subViewFolder.'/index', $viewData);
+    }
+    /* yeni kayıt işlemi*/
+    public function save()
+    {
+        $this->load->library('form_validation');
+        //kurallar
+        $news_type = $this->input->post("news_type");
+
+        if($news_type == 1){
+            if($_FILES['img_url']['name'] == ''){
+                $alert = array(
+                    'type' => 'info',
+                    'title' => 'Hata!',
+                    'message' => 'Lütfen Bir Görsel Seçiniz'
+                );
+                $this->session->set_flashdata('alert', $alert);
+                redirect(base_url('reference/new_form'));
+                die;
+            }
+        } elseif($news_type == 2){
+            $this->form_validation->set_rules('video_url', 'Video URL', 'required|trim');
+        }
+
+        $this->form_validation->set_rules('title', 'Başlık', 'required|trim');
+        //mesajlar
+        $this->form_validation->set_message(
+            array(
+                'required' => "Lütfen {field} Alanını Doldurun"
+            )
+        );
+        $validate = $this->form_validation->run();
+
+        if($validate){
+
+            if($news_type == 1){
+                $ext = pathinfo($_FILES['img_url']['name'], PATHINFO_EXTENSION);
+                $file_name = sef(pathinfo($_FILES['img_url']['name'], PATHINFO_FILENAME)).'.'.$ext;
+
+                $config = array(
+                    "allowed_types" => "jpg|jpeg|png|JPG|JPEG|PNG",
+                    "upload_path"   => "uploads/".$this->viewFolder."/",
+                    "file_name"     => $file_name,
+                );
+
+                $this->load->library("upload", $config);
+                $upload = $this->upload->do_upload("img_url");
+                if($upload){
+                    $image_url = $this->upload->data("file_name");
+                    $video_url = NULL;
+                } else{
+                    $alert = array(
+                        'type' => 'error',
+                        'title' => 'Hata!',
+                        'message' => 'Dosya Formatı JPG,PNG veya JPEG Olmalıdır'
+                    );
+                    $this->session->set_flashdata('alert', $alert);
+                    redirect(base_url('reference/new_form'));
+                    die;
+                }
+            } elseif($news_type == 2){
+                $image_url = NULL;
+                $video_url = $this->input->post('video_url');
+            } else{
+                $image_url = NULL;
+                $video_url = NULL;
+                $alert = array(
+                    'type' => 'info',
+                    'title' => 'Hata!',
+                    'message' => 'Lütfen En Az Bir Haber Türü Seçin'
+                );
+                $this->session->set_flashdata('alert', $alert);
+                redirect(base_url('reference/new_form'));
+                die;
+            }
+
+            $data = array(
+                'title'       => $this->input->post('title'),
+                'description' => $this->input->post('description'),
+                'url'         => sef($this->input->post('title')),
+                'news_type'   => $news_type,
+                'img_url'     => $image_url,
+                'video_url'   => $video_url,
+                'rank'        => 0,
+                'isActive'    => 1,
+                'createdAt'   => $this->zaman,
+            );
+            $insert = $this->reference_model->add($data);
+
+            if($insert){
+                $alert = array(
+                    'type' => 'success',
+                    'title' => 'Başarılı',
+                    'message' => 'Haber Başarıyla Eklendi'
+                );
+            } else{
+                $alert = array(
+                    'type' => 'error',
+                    'title' => 'Hata!',
+                    'message' => 'Haber Eklenemedi'
+                );
+            }
+            $this->session->set_flashdata('alert', $alert);
+            redirect(base_url('reference'));
+        } else{
+            $viewData = new stdClass();
+
+            $viewData->viewFolder = $this->viewFolder;
+            $viewData->subViewFolder = "add";
+            $viewData->form_error = TRUE;
+            $viewData->news_type = $news_type;
+
+            $this->load->view($viewData->viewFolder.'/'.$viewData->subViewFolder.'/index', $viewData);
+        }
+    }
+    /* güncelleme işlemi */
+    public function edit($id)
+    {
+        $this->load->library('form_validation');
+        //kurallar
+        $news_type = $this->input->post("news_type");
+
+        if($news_type == 2){
+            $this->form_validation->set_rules('video_url', 'Video URL', 'required|trim');
+        }
+
+        $this->form_validation->set_rules('title', 'Başlık', 'required|trim');
+        //mesajlar
+        $this->form_validation->set_message(
+            array(
+                'required' => "Lütfen {field} Alanını Doldurun"
+            )
+        );
+        $validate = $this->form_validation->run();
+
+        if($validate){
+
+            if($news_type == 1){
+                if($_FILES['img_url']['name'] != ''){
+                    $ext = pathinfo($_FILES['img_url']['name'], PATHINFO_EXTENSION);
+                    $file_name = sef(pathinfo($_FILES['img_url']['name'], PATHINFO_FILENAME)).'.'.$ext;
+
+                    $config = array(
+                        "allowed_types" => "jpg|jpeg|png|JPG|JPEG|PNG",
+                        "upload_path"   => "uploads/".$this->viewFolder."/",
+                        "file_name"     => $file_name,
+                    );
+
+                    $this->load->library("upload", $config);
+                    $upload = $this->upload->do_upload("img_url");
+                    if($upload){
+                        $image_url = $this->upload->data("file_name");
+                        $video_url = NULL;
+                    } else{
+                        $alert = array(
+                            'type' => 'error',
+                            'title' => 'Hata!',
+                            'message' => 'Dosya Formatı JPG,PNG veya JPEG Olmalıdır'
+                        );
+                        $this->session->set_flashdata('alert', $alert);
+                        redirect(base_url('reference/edit_form/'.$id));
+                        die;
+                    }
+                } else{
+                    $video_url = NULL;
+                    $image_url = $this->input->post("old_img_url");
+                }
+            } elseif($news_type == 2){
+                $image_url = NULL;
+                $video_url = $this->input->post('video_url');
+            } else{
+                $image_url = NULL;
+                $video_url = NULL;
+                $alert = array(
+                    'type' => 'info',
+                    'title' => 'Hata!',
+                    'message' => 'Lütfen En Az Bir Haber Türü Seçin'
+                );
+                $this->session->set_flashdata('alert', $alert);
+                redirect(base_url('reference/edit_form/'.$id));
+                die;
+            }
+
+            $data = array(
+                'title'       => $this->input->post('title'),
+                'description' => $this->input->post('description'),
+                'url'         => sef($this->input->post('title')),
+                'news_type'   => $news_type,
+                'img_url'     => $image_url,
+                'video_url'   => $video_url,
+            );
+            $where = array('id' => $id);
+            $update = $this->reference_model->edit($where, $data);
+
+            if($update){
+                $alert = array(
+                    'type' => 'success',
+                    'title' => 'Başarılı',
+                    'message' => 'Haber Başarıyla Güncellendi'
+                );
+            } else{
+                $alert = array(
+                    'type' => 'error',
+                    'title' => 'Hata!',
+                    'message' => 'Haber Güncellenemedi'
+                );
+            }
+            $this->session->set_flashdata('alert', $alert);
+            redirect(base_url('reference'));
+        } else{
+            $viewData = new stdClass();
+            /** Tablodan verilerin getirilmesi */
+            $item = $this->reference_model->get(
+                array(
+                    'id' => strip_tags(str_replace(' ', '', $id))
+                )
+            );
+            $viewData->viewFolder = $this->viewFolder;
+            $viewData->subViewFolder = "edit";
+            $viewData->form_error = TRUE;
+            $viewData->news_type = $news_type;
+            $viewData->item = $item;
+
+            $this->load->view($viewData->viewFolder.'/'.$viewData->subViewFolder.'/index', $viewData);
+        }
+    }
+    /* silme işlemi */
+    public function delete($id)
+    {
+        $where = array(
+            'id' => strip_tags(str_replace(' ', '', $id))
+        );
+        $delete = $this->reference_model->delete($where);
+
+        if($delete){
+            $alert = array(
+                'type' => 'success',
+                'title' => 'Başarılı',
+                'message' => 'Haber Başarıyla Silindi'
+            );
+        } else{
+            $alert = array(
+                'type' => 'error',
+                'title' => 'Hata!',
+                'message' => 'Haber Silinemedi'
+            );
+        }
+        echo json_encode($alert);
+        die;
+    }
+    /* durum değiştirme işlemi */
+    public function change_status($id)
+    {
+        if($id){
+            $status = $this->input->post('status');
+            $where = array('id' => $id);
+            $data = array('isActive' => $status);
+            $update = $this->reference_model->edit($where, $data);
+            if($update){
+                $alert = array(
+                    'type' => 'success',
+                    'title' => 'Başarılı',
+                    'message' => 'Haber Durumu Güncellendi'
+                );
+            } else{
+                $alert = array(
+                    'type' => 'error',
+                    'title' => 'Hata!',
+                    'message' => 'Haber Durumu Güncellenemedi'
+                );
+            }
+            echo json_encode($alert);
+            die;
+        }
+    }
+    /* sıralama işlemi*/
+    public function sort()
+    {
+        $data = $this->input->post('data');
+        parse_str($data, $order);
+        $items = $order['sort'];
+        $eklenen = 0;
+        $eklenmeyen = 0;
+        foreach($items as $key => $value){
+            $where = array('id' => $value);
+            $sort_data = array('rank' => $key+1);
+            $get_item = $this->reference_model->get($where);
+            if($sort_data['rank'] != $get_item->rank){
+                $update = $this->reference_model->edit($where, $sort_data);
+                if($update){
+                    $eklenen++;
+                } else{
+                    $eklenmeyen++;
+                }
+            }
+        }
+        if($eklenmeyen == 0){
+            $alert = array(
+                'type' => 'success',
+                'title' => 'Başarılı',
+                'message' => 'Haberler Başarıyla Sıralandı'
+            );
+        } else{
+            $alert = array(
+                'type' => 'error',
+                'title' => 'Hata!',
+                'message' => 'Haberler Sıralanamadı'
+            );
+        }
+        echo json_encode($alert);
+        die;
+    }
+}
---
+++ panel/application/views/reference_v/edit/content.php
@@ -0,0 +1,93 @@
+<div class="row">
+    <div class="col-md-12">
+        <h4 class="m-b-lg">Haber Düzenle</h4>
+    </div><!-- END column -->
+    <div class="col-md-12">
+        <div class="widget">
+            <div class="widget-body">
+                <form action="<?php echo base_url('news/edit/'.$item->id); ?>" method="post" enctype="multipart/form-data">
+                    <div class="form-group">
+                        <label>Başlık</label>
+                        <input type="text" name="title" class="form-control" placeholder="Başlık" value="<?php echo $item->title; ?>">
+                        <?php
+                        if(isset($form_error)){ ?>
+                            <span class="pull-right input-form-errors"><?php echo form_error('title'); ?></span>
+                            <?php
+                        }
+                        ?>
+                    </div>
+                    <div class="form-group">
+                        <label>Açıklama</label>
+                        <textarea name="description" class="m-0" data-plugin="summernote" data-options="{height: 150}"><?php echo $item->description; ?></textarea>
+                    </div>
+
+                    <?php
+                    if(isset($form_error)){
+                        if($news_type == 1){
+                            $image_class = NULL;
+                            $video_class = ' hidden';
+                            $image_selected = ' selected';
+                            $video_selected = NULL;
+                        } else{
+                            $image_class = ' hidden';
+                            $video_class = NULL;
+                            $image_selected = NULL;
+                            $video_selected = ' selected';
+                        }
+                    } else{
+                        if($item->news_type == 1){
+                            $image_class = NULL;
+                            $video_class = ' hidden';
+                            $image_selected = ' selected';
+                            $video_selected = NULL;
+                        } else{
+                            $image_class = ' hidden';
+                            $video_class = NULL;
+                            $image_selected = NULL;
+                            $video_selected = ' selected';
+                        }
+                    }
+                    ?>
+
+                    <div class="form-group">
+                        <label>Haber Türü</label>
+                        <select name="news_type" class="form-control news-type">
+                            <option<?php echo $image_selected; ?> value="1">Resim</option>
+                            <option<?php echo $video_selected; ?> value="2">Video</option>
+                        </select>
+                    </div>
+                    
+                    <div class="row image-container<?php echo $image_class; ?>">
+                        <div class="form-group col-md-1">
+                            <img src="<?php echo base_url('uploads/'.$viewFolder.'/'.$item->img_url); ?>" alt="<?php echo $item->img_url; ?>" class="img-responsive">
+                        </div>
+                        <div class="form-group col-md-11">
+                            <label>Görsel Seçiniz</label>
+                            <input type="hidden" name="old_img_url" value="<?php echo $item->img_url; ?>">
+                            <input type="file" name="img_url" class="form-control">
+                            <?php
+                            if(isset($form_error)){ ?>
+                                <span class="pull-right input-form-errors"><?php echo form_error('img_url'); ?></span>
+                                <?php
+                            }
+                            ?>
+                        </div>
+                    </div>
+                    
+                    <div class="form-group video-container<?php echo $video_class; ?>">
+                        <label>Video URL</label>
+                        <input type="text" name="video_url" class="form-control" placeholder="Video Bağlantısını Buraya Giriniz" value="<?php echo $item->video_url; ?>">
+                        <?php
+                        if(isset($form_error)){ ?>
+                            <span class="pull-right input-form-errors"><?php echo form_error('video_url'); ?></span>
+                            <?php
+                        }
+                        ?>
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-md btn-outline">Güncelle</button>
+                    <a href="<?php echo base_url('news'); ?>" class="btn btn-md btn-danger btn-outline">İptal</a>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
