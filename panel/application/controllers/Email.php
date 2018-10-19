1630a20dcd0ba2c03a31dc7c2ec928be2b76411b
Celal Akyüz <celal.akyuz@creapeak.com>
2018-10-19 23:50:56 +0300

    cms

--- panel/application/controllers/Email.php
+++ panel/application/controllers/Email.php
@@ -123,29 +123,20 @@ class Email extends CI_Controller
     public function edit($id)
     {
         $this->load->library('form_validation');
-        /** Tablodan verilerin getirilmesi */
-        $item = $this->email_model->get(
-            array(
-                'id' => strip_tags(str_replace(' ', '', $id))
-            )
-        );
         //kurallar
-
-        if($item->user_name != $this->input->post('user_name')){
-            $this->form_validation->set_rules('user_name', 'Kullanıcı Adı', 'required|trim|is_unique[users.user_name]');
-        }
-        if($item->email != $this->input->post('email')){
-            $this->form_validation->set_rules('email', 'E-Posta', 'required|trim|valid_email|is_unique[users.email]');
-        }
-
-        $this->form_validation->set_rules('full_name', 'Ad Soyad', 'required|trim');
-
+        $this->form_validation->set_rules('protocol', 'Protokol Numarası', 'required|trim');
+        $this->form_validation->set_rules('host', 'E-Posta Sunucusu', 'required|trim');
+        $this->form_validation->set_rules('port', 'Port Numarası', 'required|trim');
+        $this->form_validation->set_rules('user_name', 'Başlık', 'required|trim');
+        $this->form_validation->set_rules('user', 'E-Posta (User)', 'required|trim|valid_email');
+        $this->form_validation->set_rules('from', 'Kimden Gidecek (From)', 'required|trim|valid_email');
+        $this->form_validation->set_rules('to', 'Kime Gidecek (To)', 'required|trim|valid_email');
+        $this->form_validation->set_rules('password', 'Şifre', 'required|trim');
         //mesajlar
         $this->form_validation->set_message(
             array(
                 'required'    => "Lütfen <b>{field}</b> Alanını Doldurun",
                 'valid_email' => "Lütfen Geçerli <b>{field}</b> Adresi Girin",
-                'is_unique'   => "<b>{field}</b> Daha Önceden Kullanılmış",
             )
         );
         $validate = $this->form_validation->run();
@@ -153,9 +144,14 @@ class Email extends CI_Controller
         if($validate){
 
             $data = array(
+                'protocol'   => $this->input->post('protocol'),
+                'host'       => $this->input->post('host'),
+                'port'       => $this->input->post('port'),
                 'user_name'  => $this->input->post('user_name'),
-                'full_name'  => $this->input->post('full_name'),
-                'email'      => $this->input->post('email'),
+                'user'       => $this->input->post('user'),
+                'from'       => $this->input->post('from'),
+                'to'         => $this->input->post('to'),
+                'password'   => $this->input->post('password'),
             );
             $where = array('id' => $id);
             $update = $this->email_model->edit($where, $data);
@@ -164,19 +160,25 @@ class Email extends CI_Controller
                 $alert = array(
                     'type' => 'success',
                     'title' => 'Başarılı',
-                    'message' => 'Kullanıcı Başarıyla Güncellendi'
+                    'message' => 'E-Mail Ayarı Başarıyla Güncellendi'
                 );
             } else{
                 $alert = array(
                     'type' => 'error',
                     'title' => 'Hata!',
-                    'message' => 'Kullanıcı Güncellenemedi'
+                    'message' => 'E-Mail Ayarı Güncellenemedi'
                 );
             }
             $this->session->set_flashdata('alert', $alert);
             redirect(base_url('email'));
         } else{
             $viewData = new stdClass();
+            /** Tablodan verilerin getirilmesi */
+            $item = $this->email_model->get(
+                array(
+                    'id' => strip_tags(str_replace(' ', '', $id))
+                )
+            );
             $viewData->viewFolder = $this->viewFolder;
             $viewData->subViewFolder = "edit";
             $viewData->form_error = TRUE;
@@ -197,13 +199,13 @@ class Email extends CI_Controller
             $alert = array(
                 'type' => 'success',
                 'title' => 'Başarılı',
-                'message' => 'Kullanıcı Başarıyla Silindi'
+                'message' => 'E-Mail Ayarı Başarıyla Silindi'
             );
         } else{
             $alert = array(
                 'type' => 'error',
                 'title' => 'Hata!',
-                'message' => 'Kullanıcı Silinemedi'
+                'message' => 'E-Mail Ayarı Silinemedi'
             );
         }
         echo json_encode($alert);
@@ -221,13 +223,13 @@ class Email extends CI_Controller
                 $alert = array(
                     'type' => 'success',
                     'title' => 'Başarılı',
-                    'message' => 'Kullanıcı Durumu Güncellendi'
+                    'message' => 'E-Mail Ayarı Durumu Güncellendi'
                 );
             } else{
                 $alert = array(
                     'type' => 'error',
                     'title' => 'Hata!',
-                    'message' => 'Kullanıcı Durumu Güncellenemedi'
+                    'message' => 'E-Mail Ayarı Durumu Güncellenemedi'
                 );
             }
             echo json_encode($alert);
--- panel/application/views/email_v/edit/content.php
+++ panel/application/views/email_v/edit/content.php
@@ -1,14 +1,84 @@
 <div class="row">
     <div class="col-md-12">
-        <h4 class="m-b-lg">Referans Düzenle</h4>
+        <h4 class="m-b-lg">E-Mail Ayar Düzenle</h4>
     </div><!-- END column -->
     <div class="col-md-12">
         <div class="widget">
             <div class="widget-body">
-                <form action="<?php echo base_url('user/edit/'.$item->id); ?>" method="post">
+                <form action="<?php echo base_url('email/edit/'.$item->id); ?>" method="post">
                     <div class="form-group">
-                        <label>Kullanıcı Adı</label>
-                        <input type="text" name="user_name" class="form-control" placeholder="Kullanıcı adını giriniz" value="<?php echo isset($form_error) ? set_value("user_name") : $item->user_name; ?>">
+                        <label>Protokol</label>
+                        <input type="text" name="protocol" class="form-control" placeholder="Protokol" value="<?php echo isset($form_error) ? set_value("protocol") : $item->protocol; ?>">
+                        <?php
+                        if(isset($form_error)){ ?>
+                            <span class="pull-right input-form-errors"><?php echo form_error('protocol'); ?></span>
+                            <?php
+                        }
+                        ?>
+                    </div>
+                    <div class="form-group">
+                        <label>E-Posta Sunucu Bilgisi</label>
+                        <input type="text" name="host" class="form-control" placeholder="Hostname" value="<?php echo isset($form_error) ? set_value("host") : $item->host; ?>">
+                        <?php
+                        if(isset($form_error)){ ?>
+                            <span class="pull-right input-form-errors"><?php echo form_error('host'); ?></span>
+                            <?php
+                        }
+                        ?>
+                    </div>
+                    <div class="form-group">
+                        <label>Port Numarası</label>
+                        <input type="text" name="port" class="form-control" placeholder="Port Numarası" value="<?php echo isset($form_error) ? set_value("port") : $item->port; ?>">
+                        <?php
+                        if(isset($form_error)){ ?>
+                            <span class="pull-right input-form-errors"><?php echo form_error('port'); ?></span>
+                            <?php
+                        }
+                        ?>
+                    </div>
+                    <div class="form-group">
+                        <label>E-Posta Adresi (User)</label>
+                        <input type="email" name="user" class="form-control" placeholder="User" value="<?php echo isset($form_error) ? set_value("user") : $item->user; ?>">
+                        <?php
+                        if(isset($form_error)){ ?>
+                            <span class="pull-right input-form-errors"><?php echo form_error('user'); ?></span>
+                            <?php
+                        }
+                        ?>
+                    </div>
+                    <div class="form-group">
+                        <label>E-Posta Adresine Ait Şifre</label>
+                        <input type="password" name="password" class="form-control" placeholder="Şifre" value="<?php echo isset($form_error) ? set_value("password") : $item->password; ?>">
+                        <?php
+                        if(isset($form_error)){ ?>
+                            <span class="pull-right input-form-errors"><?php echo form_error('password'); ?></span>
+                            <?php
+                        }
+                        ?>
+                    </div>
+                    <div class="form-group">
+                        <label>Kimden Gidecek (From)</label>
+                        <input type="email" name="from" class="form-control" placeholder="From" value="<?php echo isset($form_error) ? set_value("from") : $item->from; ?>">
+                        <?php
+                        if(isset($form_error)){ ?>
+                            <span class="pull-right input-form-errors"><?php echo form_error('from'); ?></span>
+                            <?php
+                        }
+                        ?>
+                    </div>
+                    <div class="form-group">
+                        <label>Kime Gidecek (To)</label>
+                        <input type="email" name="to" class="form-control" placeholder="To" value="<?php echo isset($form_error) ? set_value("to") : $item->to; ?>">
+                        <?php
+                        if(isset($form_error)){ ?>
+                            <span class="pull-right input-form-errors"><?php echo form_error('to'); ?></span>
+                            <?php
+                        }
+                        ?>
+                    </div>
+                    <div class="form-group">
+                        <label>E-Posta Başlık</label>
+                        <input type="text" name="user_name" class="form-control" placeholder="Başlık" value="<?php echo isset($form_error) ? set_value("user_name") : $item->user_name; ?>">
                         <?php
                         if(isset($form_error)){ ?>
                             <span class="pull-right input-form-errors"><?php echo form_error('user_name'); ?></span>
@@ -16,28 +86,8 @@
                         }
                         ?>
                     </div>
-                    <div class="form-group">
-                        <label>Ad Soyad</label>
-                        <input type="text" name="full_name" class="form-control" placeholder="Ad soyad giriniz" value="<?php echo isset($form_error) ? set_value("full_name") : $item->full_name; ?>">
-                        <?php
-                        if(isset($form_error)){ ?>
-                            <span class="pull-right input-form-errors"><?php echo form_error('full_name'); ?></span>
-                            <?php
-                        }
-                        ?>
-                    </div>
-                    <div class="form-group">
-                        <label>E-Posta Adresi</label>
-                        <input type="email" name="email" class="form-control" placeholder="E-posta adresini giriniz" value="<?php echo isset($form_error) ? set_value("email") : $item->email; ?>">
-                        <?php
-                        if(isset($form_error)){ ?>
-                            <span class="pull-right input-form-errors"><?php echo form_error('email'); ?></span>
-                            <?php
-                        }
-                        ?>
-                    </div>
                     <button type="submit" class="btn btn-primary btn-md btn-outline">Güncelle</button>
-                    <a href="<?php echo base_url('user'); ?>" class="btn btn-md btn-danger btn-outline">İptal</a>
+                    <a href="<?php echo base_url('email'); ?>" class="btn btn-md btn-danger btn-outline">İptal</a>
                 </form>
             </div>
         </div>
