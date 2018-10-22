23898872afcd9dfff85619039168024f2ea4bd9d
Celal Akyüz <celal.akyuz@creapeak.com>
2018-10-22 23:58:58 +0300

    cms

--- site/application/config/routes.php
+++ site/application/config/routes.php
@@ -52,3 +52,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'blank';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+$route['urun-listesi'] = 'home/product_list';
