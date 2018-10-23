ce8330ec5fce27b397a836dace2e6eee7d80d054
Celal Akyüz <celal.akyuz@creapeak.com>
2018-10-23 23:49:13 +0300

    cms

--- site/application/controllers/Home.php
+++ site/application/controllers/Home.php
@@ -17,6 +17,11 @@ class Home extends CI_Controller{
 
     public function product_list()
     {
-
+        $viewData = new stdClass();
+        $viewData->viewFolder = "product_list_v";
+        $this->load->model('product_model');
+        $this->load->helper('text');
+        $viewData->products = $this->product_model->get_all(array('isActive' => 1), "rank ASC");
+        $this->load->view($viewData->viewFolder, $viewData);
     }
 }
---
+++ site/application/helpers/tools_helper.php
@@ -0,0 +1,22 @@
+<?php
+
+function get_product_cover_image($prd_id){
+    $t = &get_instance();
+    $t->load->model('product_image_model');
+    $cover_image = $t->product_image_model->get(
+        array(
+            'isCover'    => 1,
+            'product_id' => $prd_id,
+        )
+    );
+
+    if(empty($cover_image)){
+        $cover_image = $t->product_image_model->get(
+            array(
+                'product_id' => $prd_id
+            )
+        );
+    }
+
+    return !empty($cover_image) ? $cover_image->img_url : NULL;
+}
