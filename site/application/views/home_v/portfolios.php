789a6f9b2626a2c5408a5f901b5eb8390b2d1e88
Celal Akyüz <celal.akyuz@creapeak.com>
2018-11-13 22:35:41 +0300

    cms

---
+++ site/application/views/home_v/portfolios.php
@@ -0,0 +1,106 @@
+<section class="clearfix pv-40">
+    <div class="container">
+        <div class="row justify-content-lg-center">
+
+            <h3>Latest <strong>Projects</strong></h3>
+            <div class="separator-2"></div>
+            <div class="row grid-space-10">
+                <div class="col-sm-6 col-md-3">
+                    <div class="image-box style-2 mb-20 bordered text-center">
+                        <div id="carousel-portfolio" class="carousel slide" data-ride="carousel">
+                            <!-- Indicators -->
+                            <ol class="carousel-indicators top">
+                                <li data-target="#carousel-portfolio" data-slide-to="0" class="active"></li>
+                                <li data-target="#carousel-portfolio" data-slide-to="1"></li>
+                                <li data-target="#carousel-portfolio" data-slide-to="2"></li>
+                            </ol>
+
+                            <!-- Wrapper for slides -->
+                            <div class="carousel-inner" role="listbox">
+                                <div class="item active">
+                                    <div class="overlay-container">
+                                        <img src="<?php echo base_url('assets/images'); ?>/portfolio-1.jpg" alt="">
+                                        <div class="overlay-to-top">
+                                            <p class="small margin-clear"><em>Web design <br> Lorem ipsum dolor sit</em></p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="item">
+                                    <div class="overlay-container">
+                                        <img src="<?php echo base_url('assets/images'); ?>/portfolio-1-2.jpg" alt="">
+                                        <div class="overlay-to-top">
+                                            <p class="small margin-clear"><em>Web design <br> Lorem ipsum dolor sit</em></p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="item">
+                                    <div class="overlay-container">
+                                        <img src="<?php echo base_url('assets/images'); ?>/portfolio-1-3.jpg" alt="">
+                                        <div class="overlay-to-top">
+                                            <p class="small margin-clear"><em>Web design <br> Lorem ipsum dolor sit</em></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="body shadow light-gray-bg ">
+                            <h3>Project Title</h3>
+                            <div class="separator"></div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam atque ipsam nihil, adipisci rem minus? Voluptatem distinctio laborum porro aspernatur.</p>
+                            <a href="portfolio-item.html" class="btn btn-default btn-sm btn-hvr hvr-shutter-out-horizontal margin-clear">Read More<i class="fa fa-arrow-right pl-10"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3">
+                    <div class="image-box style-2 mb-20 shadow bordered light-gray-bg text-center">
+                        <div class="overlay-container">
+                            <img src="<?php echo base_url('assets/images'); ?>/portfolio-2.jpg" alt="">
+                            <div class="overlay-to-top">
+                                <p class="small margin-clear"><em>App development <br> Lorem ipsum dolor sit</em></p>
+                            </div>
+                        </div>
+                        <div class="body">
+                            <h3>Project Title</h3>
+                            <div class="separator"></div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam atque ipsam nihil, adipisci rem minus? Voluptatem distinctio laborum porro aspernatur.</p>
+                            <a href="portfolio-item.html" class="btn btn-default btn-sm btn-hvr hvr-shutter-out-horizontal margin-clear">Read More<i class="fa fa-arrow-right pl-10"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="clearfix visible-sm"></div>
+                <div class="col-sm-6 col-md-3">
+                    <div class="image-box style-2 mb-20 shadow bordered light-gray-bg text-center">
+                        <div class="overlay-container">
+                            <img src="<?php echo base_url('assets/images'); ?>/portfolio-3.jpg" alt="">
+                            <div class="overlay-to-top">
+                                <p class="small margin-clear"><em>App development <br> Lorem ipsum dolor sit</em></p>
+                            </div>
+                        </div>
+                        <div class="body">
+                            <h3>Project Title</h3>
+                            <div class="separator"></div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam atque ipsam nihil, adipisci rem minus? Voluptatem distinctio laborum porro aspernatur.</p>
+                            <a href="portfolio-item.html" class="btn btn-default btn-sm btn-hvr hvr-shutter-out-horizontal margin-clear">Read More<i class="fa fa-arrow-right pl-10"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3">
+                    <div class="image-box style-2 mb-20 shadow bordered light-gray-bg text-center">
+                        <div class="overlay-container">
+                            <img src="<?php echo base_url('assets/images'); ?>/portfolio-4.jpg" alt="">
+                            <div class="overlay-to-top">
+                                <p class="small margin-clear"><em>Web design <br> Lorem ipsum dolor sit</em></p>
+                            </div>
+                        </div>
+                        <div class="body">
+                            <h3>Project Title</h3>
+                            <div class="separator"></div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam atque ipsam nihil, adipisci rem minus? Voluptatem distinctio laborum porro aspernatur.</p>
+                            <a href="portfolio-item.html" class="btn btn-default btn-sm btn-hvr hvr-shutter-out-horizontal margin-clear">Read More<i class="fa fa-arrow-right pl-10"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
