5b01aaf55b0db7e3de6421fd0f39c73027af91d8
Celal Akyüz <celal.akyuz@creapeak.com>
2018-11-25 21:47:53 +0300

    cms

--- site/application/views/home_v/services.php
+++ site/application/views/home_v/services.php
@@ -1,38 +1,43 @@
-<section class="light-gray-bg pv-40 border-clear">
-    <div class="container">
+<?php
+if(!empty($services)){ ?>
+    <section class="light-gray-bg pv-40 border-clear">
+        <div class="container">
 
-        <!-- page-title start -->
-        <!-- ================ -->
-        <h1 class="page-title text-center"><strong>Hizmetlerimiz</strong></h1>
-        <div class="separator"></div>
-        <br>
-        <!-- page-title end -->
+            <!-- page-title start -->
+            <!-- ================ -->
+            <h1 class="page-title text-center"><strong>Hizmetlerimiz</strong></h1>
+            <div class="separator"></div>
+            <br>
+            <!-- page-title end -->
 
-        <div class="row">
-            <?php
-            foreach($services as $service){ ?>
-                <div class="col-sm-4">
-                    <div class="image-box style-2 mb-20">
-                        <div class="overlay-container overlay-visible">
-                            <img src="<?php echo get_media("service_v", $service->img_url, "350x217"); ?>" alt="<?php echo $service->title; ?>">
-                            <a href="<?php echo base_url("hizmet-detay/$service->url"); ?>" class="overlay-link"><i class="fa fa-link"></i></a>
-                            <div class="overlay-bottom">
-                                <div class="text">
-                                    <p class="lead margin-clear text-left mobile-visible"><?php echo $service->title; ?></p>
+            <div class="row">
+                <?php
+                foreach($services as $service){ ?>
+                    <div class="col-sm-4">
+                        <div class="image-box style-2 mb-20">
+                            <div class="overlay-container overlay-visible">
+                                <img src="<?php echo get_media("service_v", $service->img_url, "350x217"); ?>" alt="<?php echo $service->title; ?>">
+                                <a href="<?php echo base_url("hizmet-detay/$service->url"); ?>" class="overlay-link"><i class="fa fa-link"></i></a>
+                                <div class="overlay-bottom">
+                                    <div class="text">
+                                        <p class="lead margin-clear text-left mobile-visible"><?php echo $service->title; ?></p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="body padding-horizontal-clear">
-                            <p><?php echo character_limiter(strip_tags($service->description), 200); ?></p>
-                            <a class="link-dark" href="<?php echo base_url("hizmet-detay/$service->url"); ?>">Devam Et</a>
+                            <div class="body padding-horizontal-clear">
+                                <p><?php echo character_limiter(strip_tags($service->description), 200); ?></p>
+                                <a class="link-dark" href="<?php echo base_url("hizmet-detay/$service->url"); ?>">Devam Et</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-            <?php
-            }
-            ?>
+                    <?php
+                }
+                ?>
+
+            </div>
 
         </div>
-
-    </div>
-</section>
+    </section>
+<?php
+}
+?>
