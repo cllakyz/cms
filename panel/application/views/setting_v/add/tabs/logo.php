8417e5bb77d9a24e32192238238494e008592027
Celal Akyüz <celal.akyuz@creapeak.com>
2018-11-29 22:59:12 +0300

    cms

--- panel/application/views/setting_v/add/tabs/logo.php
+++ panel/application/views/setting_v/add/tabs/logo.php
@@ -1,8 +1,20 @@
 <div role="tabpanel" class="tab-pane fade" id="tab-7">
     <div class="row">
         <div class="form-group col-md-6">
-            <label>Görsel Seçiniz</label>
+            <label>Logo Seçiniz</label>
             <input type="file" name="logo" class="form-control">
+        </div>
+    </div>
+    <div class="row">
+        <div class="form-group col-md-6">
+            <label>Mobil Logo Seçiniz</label>
+            <input type="file" name="logo_mobile" class="form-control">
+        </div>
+    </div>
+    <div class="row">
+        <div class="form-group col-md-6">
+            <label>Favicon Seçiniz</label>
+            <input type="file" name="favicon" class="form-control">
         </div>
     </div>
 </div>
--- panel/application/views/setting_v/edit/tabs/logo.php
+++ panel/application/views/setting_v/edit/tabs/logo.php
@@ -1,12 +1,34 @@
 <div role="tabpanel" class="tab-pane fade" id="tab-7">
     <div class="row">
-        <div class="col-md-3">
-            <img src="<?php echo get_media($viewFolder, $item->logo, "150x35"); ?>" alt="<?php echo $item->company_name; ?>" class="img-responsive">
+        <div class="col-md-2">
+            <img src="<?php echo get_media($viewFolder, $item->logo, "150x35"); ?>" alt="<?php echo $item->company_name; ?>" class="img-responsive logo-img">
         </div>
         <div class="form-group col-md-6">
-            <label>Görsel Seçiniz</label>
+            <label>Logo Seçiniz</label>
             <input type="hidden" name="old_logo" value="<?php echo $item->logo; ?>">
             <input type="file" name="logo" class="form-control">
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-2">
+            <img src="<?php echo get_media($viewFolder, $item->logo_mobile, "150x35"); ?>" alt="<?php echo $item->company_name; ?>" class="img-responsive logo-img">
+        </div>
+        <div class="form-group col-md-6">
+            <label>Mobil Logo Seçiniz</label>
+            <input type="hidden" name="old_logo_mobile" value="<?php echo $item->logo_mobile; ?>">
+            <input type="file" name="logo_mobile" class="form-control">
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-2">
+            <img src="<?php echo get_media($viewFolder, $item->favicon, "150x35"); ?>" alt="<?php echo $item->company_name; ?>" class="img-responsive logo-img">
+        </div>
+        <div class="form-group col-md-6">
+            <label>Favicon Seçiniz</label>
+            <input type="hidden" name="old_favicon" value="<?php echo $item->favicon; ?>">
+            <input type="file" name="favicon" class="form-control">
         </div>
     </div>
 </div>
