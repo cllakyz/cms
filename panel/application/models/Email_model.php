d66f6d7fda0d167d11edd34eabc777796b7674f2
Celal Akyüz <celal.akyuz@creapeak.com>
2018-12-11 22:55:16 +0300

    cms

--- panel/application/models/Email_model.php
+++ panel/application/models/Email_model.php
@@ -1,42 +1,9 @@
 <?php
-class Email_model extends CI_Model
+class Email_model extends VS_Model
 {
-    public $tableName = "email_settings";
-
     public function __construct()
     {
         parent::__construct();
-    }
-    /** belirtilen kaydı getirir
-     * @param array $where
-     * @return
-     */
-    public function get($where = [])
-    {
-        return $this->db->where($where)->get($this->tableName)->row();
-    }
-
-    /** tüm kayıtları listeler
-     * @param array $where
-     * @return
-     */
-    public function get_all($where = [], $order = "id ASC")
-    {
-        return $this->db->where($where)->order_by($order)->get($this->tableName)->result();
-    }
-
-    public function add($data = array())
-    {
-        return $this->db->insert($this->tableName, $data);
-    }
-
-    public function edit($where = array(), $data = array())
-    {
-        return $this->db->where($where)->update($this->tableName, $data);
-    }
-
-    public function delete($where = [])
-    {
-        return $this->db->where($where)->delete($this->tableName);
+        $this->tableName = "email_settings";
     }
 }
--- panel/application/models/Video_model.php
+++ panel/application/models/Video_model.php
@@ -1,42 +1,9 @@
 <?php
-class Video_model extends CI_Model
+class Video_model extends VS_Model
 {
-    public $tableName = "videos";
-
     public function __construct()
     {
         parent::__construct();
-    }
-    /** belirtilen kaydı getirir
-     * @param array $where
-     * @return
-     */
-    public function get($where = [])
-    {
-        return $this->db->where($where)->get($this->tableName)->row();
-    }
-
-    /** tüm kayıtları listeler
-     * @param array $where
-     * @return
-     */
-    public function get_all($where = [], $order = "id ASC")
-    {
-        return $this->db->where($where)->order_by($order)->get($this->tableName)->result();
-    }
-
-    public function add($data = array())
-    {
-        return $this->db->insert($this->tableName, $data);
-    }
-
-    public function edit($where = array(), $data = array())
-    {
-        return $this->db->where($where)->update($this->tableName, $data);
-    }
-
-    public function delete($where = [])
-    {
-        return $this->db->where($where)->delete($this->tableName);
+        $this->tableName = "videos";
     }
 }
